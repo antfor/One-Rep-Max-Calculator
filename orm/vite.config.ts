710e968be2d2b2493320e5d591b1e95aebfd7acf
antfor <38752136+antfor@users.noreply.github.com>
2026-03-05 21:34:35 +0100

    use transparent logo for linux/windows

--- orm/vite.config.ts
+++ orm/vite.config.ts
@@ -43,7 +43,7 @@ export default defineConfig({
         src: 'snake-logo-maskable-512.png',
         sizes: '512x512',
         type: 'image/png',
-        purpose: 'any maskable'
+        purpose: 'maskable'
       }
     ],
     screenshots: [
